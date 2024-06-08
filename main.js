92b759f0e824d2105ec304d717ca2535e38a853b
Herny664 <hern4n.roch0n@gmail.com>
2024-06-07 23:52:41 -0300

    Main section finished

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const menuButton = document.querySelector(".header__menu-container");
 const nav = document.querySelector(".header__nav");
 const openMenuIcon = document.querySelector(".open-menu");
 const closeMenuIcon = document.querySelector(".close-menu");
+const body = document.body;
 
 menuButton.addEventListener("click", () => {
 	if (nav.classList.contains("opened")) {
 		nav.classList.remove("opened");
 		nav.classList.add("closed");
+		body.classList.remove("no-scroll");
 	} else {
 		nav.classList.remove("closed");
 		nav.classList.add("opened");
+		body.classList.add("no-scroll");
 	}
 
 	openMenuIcon.classList.toggle("hidden");
@@ -19,8 +22,7 @@ menuButton.addEventListener("click", () => {
 const navMql = matchMedia("(min-width: 600px)");
 navMql.addEventListener("change", () => {
 	if (navMql.matches) {
-		if (nav.classList.contains("closed")) {
-			nav.classList.remove("closed");
-		}
+		nav.classList.remove("closed");
+		body.classList.remove("no-scroll");
 	}
 });
