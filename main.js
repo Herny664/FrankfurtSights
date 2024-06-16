5e25d0bc2c576b5eeaf3c8d3d6d6798ea641115a
Herny664 <hern4n.roch0n@gmail.com>
2024-06-15 22:05:13 -0300

    Responsive home

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+// Query Selectors
+const body = document.body;
 const menuButton = document.querySelector(".header__menu-container");
 const nav = document.querySelector(".header__nav");
 const openMenuIcon = document.querySelector(".open-menu");
 const closeMenuIcon = document.querySelector(".close-menu");
-const body = document.body;
 
+// Functions
+
+// Evern Listeners
 menuButton.addEventListener("click", () => {
 	if (nav.classList.contains("opened")) {
 		nav.classList.remove("opened");
